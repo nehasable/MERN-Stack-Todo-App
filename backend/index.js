581689d3cbc2e76281632d3dbd55b760d48c1f70
nehasable <nehasable45@gmail.com>
2024-02-25 15:25:04 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const app=express()
+
+app.use(express.json())
+app.post('/todos',(req,res)=>{
+
+})
+app.get("/todos",(req,res)=>{
+
+})
+app.put("/completed",(req,res)=>{
+    
+})
