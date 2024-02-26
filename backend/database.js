a00b96dc309a6d64290811ece76ccb8c99142130
nehasable <nehasable45@gmail.com>
2024-02-26 19:35:44 +0530

    commit

---
+++ backend/database.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb://localhost:27017/todo")
+const todoSchema=mongoose.Schema({
+    title:string,
+    description:string,
+    completed:boolean
+})
+const todo=mongoose.model('todos',todoSchema)
